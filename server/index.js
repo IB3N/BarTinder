bab1694779adfc1535d584bed39d7023b8593580
Ben Pearce <benpearce9@live.co.uk>
2021-03-04 22:23:58 +0000

    add server index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,31 @@
 'use strict';
 
-const Express = require('express');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+// const router = require('./router');
+
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(router);
+
+(async function () {
+  try {
+    app.listen(PORT, () => {
+      console.log(`🤟 Server running at http://localhost:${PORT} 👊`);
+    });
+    try {
+      await db.sequelize.authenticate();
+      await db.sequelize.sync();
+      console.log('🤙 You have successfully Databased 👌');
+    } catch (error) {
+      console.error(`👎 Could not connect to database: ${error} 🖕`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+})();

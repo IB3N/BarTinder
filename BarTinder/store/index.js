793f6af25e63abec6190d75e4fdb22946251d001
Ben Pearce <benpearce9@live.co.uk>
2021-03-17 21:51:04 +0000

    import cocktails reducer into store/index.js to add to combineReducers function

--- BarTinder/store/index.js
+++ BarTinder/store/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { user } from './reducers/user';
+import { cocktails } from './reducers/cocktails';
 
 // Combining both reducers
 const reducers = combineReducers({
   user,
+  cocktails,
 });
 
 export default reducers;
